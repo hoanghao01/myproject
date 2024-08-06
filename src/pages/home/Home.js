5b3679eb7f84eda8f3232be1e1b4ca4de7709f63
hoanghao01 <haonguyen6345@gmail.com>
2024-08-06 07:38:57 +0700

    first commit

---
+++ src/pages/home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from '../../components/Header'
+import './home.css'
+
+const Home = () => {
+  return (
+    <>
+    <Header />
+    Home
+    </>
+  )
+}
+
+export default Home
