6a4bfe87394fa0a78ac2e23c6ede187abf782073
hoanghao01 <haonguyen6345@gmail.com>
2024-08-08 07:07:01 +0700

    add header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,30 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom'
+import Image from '../images/main_header.png'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header className='main-header'>
+      <div className='container main-header-containner'>
+        <div className='main-header-left'>
+          <h4>#100DaysOfWorkOut</h4>
+          <h1>Join The Legends Of The Fitness World</h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+            Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </p>
+          <Link to="/plans" className='btn lg'>Get Started</Link>
+        </div>
+        <div className='main-header-right'>
+          <div className='main-header-circle'>
+
+          </div>
+          <div className='main-header-image'>
+            <img src={Image} alt='Main Header Image' />
+          </div>
+        </div>
+      </div>
+    </header>
   )
 }
 
